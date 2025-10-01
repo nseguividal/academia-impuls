4f1b10e16103e70efae96152de8fdf5a6296472a
Núria Seguí <nuria@MacBook-Nuria-nou.local>
2025-10-01 11:26:13 +0200

    Try fixing web 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,22 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/academia-impuls/', // <-- Afegeix aquesta línia
+    base: '/academia-impuls/',  // <- important perquè GitHub Pages serveixi bé
+    plugins: [react()],
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
-    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      }
-    }
   };
 });
+
 
