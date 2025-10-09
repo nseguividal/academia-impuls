f3c5086251555773989098bceadaae660cf0c802
Núria Seguí <nuria@MacBook-Nuria-nou.local>
2025-10-10 00:29:08 +0200

    Update homepage/base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/academia-impuls/',  // <- important perquè GitHub Pages serveixi bé
+    base: '/impulsbatxillerat/',  // <- important perquè GitHub Pages serveixi bé
     plugins: [react()],
     server: {
       port: 3000,
